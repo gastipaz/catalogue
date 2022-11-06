781a8277687480738ea37b57b684a4042b1fce59
Gaston Paz <gastipaz@gmail.com>
2022-11-06 12:27:54 +0100

    made changes to _init_.py

--- server/website/__init__.py
+++ server/website/__init__.py
@@ -19,19 +19,19 @@ def create_app():
     # app.config['SQLALCHEMY_DATABASE_URI']=f'sqlite:///{DB_NAME}'
     # app.config['SQLALCHEMY_DATABASE_URI']="postgres://mwxubyhjmhdthp:c8be4834d7fde9ca001ec2f58d7a8e1b2b8149cc569bad7c24ec9b2053ec9f37@ec2-176-34-215-248.eu-west-1.compute.amazonaws.com:5432/d6uemuitmqfsn1"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
-    app.config['SESSION_TYPE'] = 'sqlalchemy'
+    # app.config['SESSION_TYPE'] = 'sqlalchemy'
     db.init_app(app)
-    app.config['SESSION_SQLALCHEMY'] = db
-    sess = Session()
-    sess.init_app(app)
-    migrate = Migrate()
+    # app.config['SESSION_SQLALCHEMY'] = db
+    # sess = Session()
+    # sess.init_app(app)
+    #migrate = Migrate()
 
-    from server.website.models import ProductsTable, OrdersTable
     from server.website.views import views
     
-    migrate.init_app(app, db)
+    # migrate.init_app(app, db)
     
     app.register_blueprint(views, url_prefix='/')
+    from server.website.models import ProductsTable, OrdersTable
 
     create_database(app)    
 
